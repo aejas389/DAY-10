56ac499d6106af044fd7ea3ca2e68e4d5ef8808e
aejas389 <23hr1a3252@mtieat.org>
2024-12-11 15:34:29 +0530

    Add files via upload

---
+++ fib3.java
@@ -0,0 +1,21 @@
+ class fib3
+{
+
+      public static void main(String[] args) 
+      {
+        int a=0;
+        int b=1;
+        for(int i=0;i<0;i++)
+        {
+            int c=a+b;
+            System.out.println(i+" ->"+c);
+            a=b;
+            b=c;
+
+        }
+    }
+}
+        
+    
+  
+
