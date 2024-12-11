09474287d2bcd2a06d392e87b02cd2b7574210b5
aejas389 <23hr1a3252@mtieat.org>
2024-12-11 15:35:20 +0530

    Add files via upload

---
+++ constructer.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class a 
+{
+    int c;
+    String b;
+    a()
+    {
+        this.c=420;
+        this.b="hello aejas";
+    }
+    void display()
+    {
+        System.out.println((b+" "+c));
+    }
+}
+public class constructer
+{
+    public static void main(String[] args){
+    
+            a s = new a();
+            s.display();
+    }
+}
+    
+
+
+    
+    
+
